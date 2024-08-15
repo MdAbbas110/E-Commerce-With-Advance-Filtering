a195cf3ab2e8fb1aac792efaa4cd20054361aaad
abbas110 <meerabbas110@gmail.com>
2024-08-15 22:48:55 +0530

    popular blogs for the user info

--- src/components/PopularBlogs.tsx
+++ src/components/PopularBlogs.tsx
@@ -1,5 +1,10 @@
 const PopularBlogs = () => {
-  return <div>PopularBlogs</div>;
+  return (
+    <div>
+      <h1>Popular Blogs</h1>
+      <p>This is the popular blogs section</p>
+    </div>
+  );
 };
 
 export default PopularBlogs;
